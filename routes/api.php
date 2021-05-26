4176db79f6ca870da7dbd4b7e45c747daab77a8f
vin-albertus <vinalbertus@gmail.com>
2021-05-26 22:20:27 +0700

    initialize branch, test route api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/", function () {
+    return response()->json([
+        'status' => true,
+        'message' => 'Test api app',
+    ], 200);
+});
